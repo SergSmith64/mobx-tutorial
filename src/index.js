214197cdb1034e037cb951f1c8e10f982f297d06
SergSmith64 <sergsmith2008@yandex.ru>
2020-08-01 16:30:45 +0400

    fix

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ const counterState = observable({
   count: 0,
 });
 
-counterState.increment = function() {
+counterState.increment = function () {
   this.count++
 }
 
-counterState.decrement = function() {
+counterState.decrement = function () {
   this.count--
 }
 
